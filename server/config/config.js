a9ffa383808122ed7749fd339cea4de81acce705
Kyle Dominic Lomigo <rekkyles@pop-os.localdomain>
2023-11-22 23:25:13 +0800

    fix(server): replace set up for backend

---
+++ server/config/config.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+
+
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  }
+}
