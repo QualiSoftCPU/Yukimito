997a751647ee99edd2e92067d0dc3279fd2630f7
PaulVincentLerado17 <paulvincent.lerado-21@cpu.edu.ph>
2023-12-02 22:02:47 +0800

    Landing Page and pages routing

---
+++ client/src/pages/PetOwnerLanding.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ClientNavBar from '../components/ClientNavBar'
+
+const PetOwnerLanding = () => {
+  return (
+    <>
+    <ClientNavBar navItems={[]} />
+    <div>PetOwnerLanding</div>
+    </>
+  )
+}
+
+export default PetOwnerLanding
