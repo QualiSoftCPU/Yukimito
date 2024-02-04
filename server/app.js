17e6ac02a1a3d3f6fa453e2a6b12641837c59f41
Kyle Dominic Lomigo <rekkyles@pop-os.localdomain>
2024-02-04 13:55:42 +0800

    fix: modified app.js

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
  console.log(`Server is running on port ${PORT}.`);
 });
