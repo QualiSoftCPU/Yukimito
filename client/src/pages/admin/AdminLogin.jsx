da1f895017de7763b4c419542225b29d3cc133b4
PaulVincentLerado17 <paulvincent.lerado-21@cpu.edu.ph>
2024-04-18 00:09:02 +0800

    update: added hero to login page

--- client/src/pages/admin/AdminLogin.jsx
+++ client/src/pages/admin/AdminLogin.jsx
@@ -5,6 +5,7 @@ import Card from "@mui/material/Card";
 import NavBar from "../../components/partials/NavBar";
 import Footer from "../../components/partials/Footer";
 import axios from "axios";
+import LoginHero from "../partials/HeroLogin";
 
 const inputDetails = [
   {
@@ -70,46 +71,49 @@ export default function AdminLogin() {
 
       <div className="container my-5 main-container pt-5">
         <div className="row">
-
+          
           <div className="col-sm mt-3 d-flex justify-content-center text-center">
             <div>
-              <div className="row ">
-           
-              </div>
+              <div className="row "></div>
+              <LoginHero />
             </div>
           </div>
 
           <div className="col-sm mt-3 p-0">
-            <div className='mx-3'>
-              <Box sx={{ gridArea: 'form' }} >
-                  <Box>
-                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                      <Card className='form-bg-color form-styles shadow'>
-                        <Box className="input-container">
+            <div className="mx-3">
+              <Box sx={{ gridArea: "form" }}>
+                <Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Card className="form-bg-color form-styles shadow">
+                      <Box className="input-container">
+                        <p className="text-center h4 mb-3 black-font fw-bold">
+                          Login to Yukimito!
+                        </p>
 
-                          <p className='text-center h4 mb-3 black-font fw-bold'>
-                            Login to Yukimito!
-                          </p>
+                        {inputDetails.map((details, index) => {
+                          return (
+                            <TextField
+                              key={index}
+                              className="input-margin non-inline input-styling"
+                              onChange={handleInput}
+                              name={details.name}
+                              placeholder={details.placeholder}
+                              type={details.type}
+                              id="outlined-basic"
+                              label={details.label}
+                              variant="outlined"
+                              required
+                            />
+                          );
+                        })}
 
-                          {inputDetails.map((details, index) => {
-                            return (
-                                <TextField
-                                key={index}
-                                className='input-margin non-inline input-styling'
-                                onChange={handleInput}
-                                name={details.name}
-                                placeholder={details.placeholder}
-                                type={details.type}
-                                id="outlined-basic" 
-                                label={details.label} 
-                                variant="outlined"          
-                                required
-                              />
-                            )
-                          })}
-                       
-                          
-                          {/* {isInvalidLogin && (
+                        {/* {isInvalidLogin && (
   
                             <Alert severity="error" 
                             >
@@ -118,20 +122,24 @@ export default function AdminLogin() {
                             </Alert>
             
                           )} */}
-                          
-                          <div class="d-grid gap-2 my-2">
-                            <button class="btn btn-primary button-color" onClick={handleSubmit} type="button">Login</button>
-                          </div>
-                        
-                        </Box>
-                      </Card>
-                    </Box>
+
+                        <div class="d-grid gap-2 my-2">
+                          <button
+                            class="btn btn-primary button-color"
+                            onClick={handleSubmit}
+                            type="button"
+                          >
+                            Login
+                          </button>
+                        </div>
+                      </Box>
+                    </Card>
                   </Box>
                 </Box>
+              </Box>
             </div>
           </div>
         </div>
-
       </div>
 
       <Footer />
