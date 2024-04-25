83a6533a3dee0c4580bce3dcc6be4f4b15ce6f3b
PaulVincentLerado17 <paulvincent.lerado-21@cpu.edu.ph>
2024-04-25 20:05:41 +0800

    update: added a remove and add vaccine options

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -240,14 +240,14 @@ const AdminDashBoard = () => {
                                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                                 </svg>
                               </span>
-                              {/* <div class="d-flex justify-content-end">
+                              <div class="d-flex justify-content-end">
                                 <button class="btn btn-primary yuki-color button-border-color mx-2">
                                   Add Vaccine
                                 </button>
                                 <button class="btn btn-danger mx-2">
                                   Remove Vaccine
                                 </button>
-                              </div> */}
+                              </div>
                             </div>
                             
                          
