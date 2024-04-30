2bc75464a797ffb0ee1f0d9c9034082e5f43487f
PaulVincentLerado17 <paulvincent.lerado-21@cpu.edu.ph>
2024-04-30 15:34:40 +0800

    update: remove vaccine modal

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -298,8 +298,29 @@ const AdminDashBoard = () => {
         ...
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
         <button type="button" class="btn btn-primary yuki-color button-border-color">Add Vaccine</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+                   {/* <!-- Remove Vaccine Modal --> */}
+           
+                   <div class="modal fade" id="RemoveVaccineModal" tabindex="-1" role="dialog" aria-labelledby="RemoveVaccineModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="RemoveVaccineModalTitle">Remove Vaccine</h5>
+        
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary yuki-color button-border-color">Remove Vaccine</button>
       </div>
     </div>
   </div>
