6b1c16323090e973a3b6e061a62e4546139a0c04
Lyzza Flores <89468418+liechee@users.noreply.github.com>
2024-05-06 18:36:42 +0800

    Added main nav bar

---
+++ client/src/components/partials/admin-dashboard/AdminMainNav.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import WhatWeOffer from "../../../pages/partials/WhatWeOffer";
+import AdminDashBoard from "../../../pages/admin/AdminDashboard";
+import NavBarMain from "../../../pages/partials/NavBarMain";
+
+const AdminMainNav = () => {
+  const items = [
+    "About",
+    "Gallery",
+    "Requirements",
+    "Rates & Services",
+    "Team",
+    "Reviews",
+  ];
+  const navItems = [
+    <a
+      href="/AdminDashBoard"
+      style={{ textDecoration: "none", color: "white" }}
+    >
+      Dashboard
+    </a>,
+    ...items,
+  ];
+  return (
+    <>
+      <NavBarMain navItems={navItems} />
+      <hr id="About" className="mb-5 pb-5" />
+      <WhatWeOffer />
+    </>
+  );
+};
+
+export default AdminMainNav;
