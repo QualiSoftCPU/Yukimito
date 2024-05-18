b795e1ffe420736a710d63e12a7fc58c96861cc0
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-05-18 18:46:28 +0800

    fix: update on getting details and display petowner email

--- client/src/pages/client/PetOwnerDashboard.jsx
+++ client/src/pages/client/PetOwnerDashboard.jsx
@@ -74,8 +74,6 @@ export default function PetOwnerDashboard() {
       ...pet,
       [event.target.name]: event.target.files[0],
     });
-
-    console.log(pet);
   };
 
   const handleUploadProfilePhoto = async (event) => {
@@ -310,7 +308,7 @@ export default function PetOwnerDashboard() {
           username: userDetails.username,
           address: "No address",
           contactNumber: userDetails.contact_number,
-          email: userDetails.email,
+          email: userDetails.email_address,
           profilePhoto: userDetails.profilePhoto
         });
       })
@@ -377,7 +375,7 @@ export default function PetOwnerDashboard() {
                       username={petOwnerDetails.username}
                       contactNumber={petOwnerDetails.contactNumber}
                       address={petOwnerDetails.address}
-                      email={petOwnerDetails.email}
+                      email={petOwnerDetails}
                       openEdit={openEdit}
                       handleUpdateUser={handleUpdateUser}
                       handleUpdate={handleUpdate}
